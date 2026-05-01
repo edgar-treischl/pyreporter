f4373c6ac3213ee5d68b3d72ba759298e9a65f1b
edgar.treischl <edgar.treischl@isb.bayern.de>
2026-05-01 06:58:12 +0200

    testing great-docs

--- tester.py
+++ tester.py
@@ -1,13 +1,20 @@
-from plotnine import ggplot, aes, geom_point, facet_wrap, theme, element_text, labs
-from plotnine.data import mtcars
 
-(ggplot(mtcars, aes("wt", "mpg"))
- + geom_point()
- + facet_wrap("~gear")
- + theme(
-     # Left align the text within the strip
-     strip_text_x=element_text(ha="right"), 
-     # Optional: Align the strip background to the left edge
-     strip_align_x=0 
- )
-)
+
+from pyreporter.meta_repository import MetaRepository
+
+meta_repo = MetaRepository()
+meta_snames = meta_repo.meta_snames
+
+tmp_name = meta_snames[meta_snames['SNR'] == "0002"]
+
+    # Check if more than one name is found
+if len(tmp_name) > 1:
+    raise ValueError("Error in get_sname(): More than one school name found.")
+
+    # If no name is found
+if len(tmp_name) == 0:
+    print("School name not available.")
+
+# Return the school name (as a string)
+sname = tmp_name.iloc[0]['SNAME']
+print(sname)
